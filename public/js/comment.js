5f476d090cd2fba5c921818afa2c686d15d2617b
brett hockridge <bretthockridge98@gmail.com>
2022-05-28 10:42:35 -0700

    finished commentjs

--- public/js/comment.js
+++ public/js/comment.js
@@ -20,3 +20,70 @@ const newFormHandler = async (event) => {
       }
     }
   };
+
+  const delButtonHandler = async (event) => {
+    event.preventDefault();
+    if (event.target.hasAttribute('comment-id')) {
+      const id = event.target.getAttribute('comment-id');
+      const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to delete comment');
+      }
+    }
+  };
+  
+  const cancelButtonHandler = async (event) => {
+    event.preventDefault();
+    if (event.target.hasAttribute('cancel-id')) {
+      const blog_id = event.target.getAttribute('blog-id');
+      document.location.replace(`/blogs/${blog_id}`);
+    }
+  };
+  
+  const updateCommentHandler = async (event) => {
+    event.preventDefault();
+    if (event.target.hasAttribute('blog-id')) {
+      const id = event.target.getAttribute('update-id');
+      const description = document.querySelector('#comment-desc').value.trim();
+      const blog_id = event.target.getAttribute('blog-id');
+  
+      if (description) {
+        const response = await fetch(`/api/comments/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ description }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          document.location.replace(`/blogs/${blog_id}`);
+        } else {
+          alert('Failed to update comment');
+        }
+      }
+    }
+  };
+  
+  if (document.querySelector('.new-comment-form') != null)
+    document
+      .querySelector('.new-comment-form')
+      .addEventListener('submit', newFormHandler);
+  
+  if (document.querySelector('.update-comment-form') != null)
+    document
+      .querySelector('.update-comment-form')
+      .addEventListener('click', updateCommentHandler);
+  
+  if (document.querySelector('.comment-list') != null)
+    document
+      .querySelector('.comment-list')
+      .addEventListener('click', delButtonHandler);
+  
+  if (document.querySelector('.cancel-button') != null)
+    document
+      .querySelector('.cancel-button')
+      .addEventListener('click', cancelButtonHandler);
